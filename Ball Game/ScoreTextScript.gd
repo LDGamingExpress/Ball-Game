07ebf30fee48b0fd2a7b243e30d31abf7a66361d
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2025-09-24 17:41:57 -0400

    Globals and ScoreText Comments
    Comments were added to explain the Globals and ScoreText Scripts

--- Ball Game/ScoreTextScript.gd
+++ Ball Game/ScoreTextScript.gd
@@ -3,12 +3,11 @@ var Stage = 0
 var Score = 100
 
 func _ready() -> void:
-	text = "+" + str(Score)
+	text = "+" + str(Score)#Displays score of the text
 
 func _process(delta: float) -> void:
 	position.y -= 0.2
-	#print(self_modulate.a)
-	match(Stage):
+	match(Stage):#Tracks the "Stage" to know when to fade in and out the text
 		0:
 			self_modulate.a = lerp(self_modulate.a,1.0,0.05)
 			if self_modulate.a > 0.95:
--- Ball Game/globals.gd
+++ Ball Game/globals.gd
@@ -2,3 +2,4 @@ extends Node
 var Score = 0
 var Level = 0
 var Levels = ["res://TutorialLevel.tscn","res://Level1.tscn","res://Level2.tscn","res://Level3.tscn"]
+#Levels must contain the string of the path for every level so that the player can load them upon reaching an exit
