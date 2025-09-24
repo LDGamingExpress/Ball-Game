9d4817c327c917be9e070e822b169fb1b364684e
LDGamingExpress <110698883+LDGamingExpress@users.noreply.github.com>
2025-09-23 22:58:55 -0400

    Ragdolls react to stop ball
    Ragdolls now raise their arms to stop the ball

--- Ball Game/RagdollScript.gd
+++ Ball Game/RagdollScript.gd
@@ -1,8 +1,18 @@
 extends CharacterBody2D
 @export var Sprite = 0
+var KnockedOut = false
 
 func KnockOut():
+	KnockedOut = true
 	if $Chest/NeckJoint/Head/BirdAnimation.self_modulate.a < 1:
 		$Chest/NeckJoint/Head/BirdAnimation.self_modulate.a = lerp($Chest/NeckJoint/Head/BirdAnimation.self_modulate.a,1.0,0.1)
 		await get_tree().create_timer(0.16).timeout
 		KnockOut()
+
+
+func _on_detect_area_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player") and KnockedOut == false:
+		#$Chest/ArmLJoint/ArmL.apply_torque(500)
+		$Chest/ArmLJoint/ArmL.angular_velocity = 50
+		#$Chest/ArmRJoint/ArmR.apply_torque(-500)
+		$Chest/ArmRJoint/ArmR.angular_velocity = -50
